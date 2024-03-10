6b01827dd316416a4e44ef87b353ace47e57808d
Mitch_Jones <mitch@DESKTOP-F6M2N47>
2024-03-09 21:05:05 -0600

    finished I guess

---
+++ scraper.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup as bs
+import requests
+
+url = "https://ndawn.info/snow_photos.html"
+stakes = []
+
+def scrape():
+    data = requests.get(url).text
+    soup = bs(data, 'html.parser')
+    for item in soup.find_all('img'):
+        for name in stakes:
+            if name.capitalize() in item['src']:
+                _save_img(item['src'])
+                break
+        
+def read_stakes():
+    file = open('.\stakes.txt')
+    for line in file:
+        if not is_blank(line):
+            stakes.append(line.rstrip().lower())
+    file.close()
+
+def is_blank(line: str) -> bool:
+    line = line.replace(" ", "")
+    line = line.rstrip()
+    return len(line) == 0
+
+def _save_img(url: str):
+    data = requests.get(url).content
+    img = open(f'img/{url[66:]}', 'wb')
+    img.write(data)
+    img.close
+
+if __name__ == "__main__":
+    read_stakes()
+    scrape()
